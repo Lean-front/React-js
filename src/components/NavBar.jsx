39e8a5fca8e2c752f638675805074ca3730de712
Leandro Benavides <leandrobenavi1997@hotmail.com>
2022-01-11 12:45:09 -0300

    desafio2

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <>
+            <ul>
+                <li>INICIO</li>
+                <li>TIENDA</li>
+                <li>SOBRE NOSOTROS</li>
+                <li>CONTACTO</li>
+            </ul>
+        </>
+    )
+}
